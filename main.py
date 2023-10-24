aeadd66b19d733b647922c327179bff4af9c7fc2
liske1 <liske1@linuxsystems.ovh>
2023-10-24 14:27:37 +0200

    add first import library and example from where we get this code

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
-print("First AI project")
+# Example from https://www.toptal.com/data-science/machine-learning-number-recognition
+
+from sklearn.datasets import load_digits
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.model_selection import train_test_split, cross_val_score
+import numpy as np
+import matplotlib.pyplot as plt
+
