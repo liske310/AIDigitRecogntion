3d3ab65b6df49f8e569efb6dab7afb3fb3e6b810
liske1 <liske1@linuxsystems.ovh>
2023-10-27 11:48:38 +0200

    push tutorial code

--- main.py
+++ main.py
@@ -1,3 +1,44 @@
 # Example from https://www.toptal.com/data-science/machine-learning-number-recognition
 # Example from https://www.youtube.com/watch?v=bte8Er0QhDg
 
+import os
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+import tensorflow as tf
+
+
+mnist = tf.keras.datasets.mnist
+(x_train, y_train), (x_test, y_test) = mnist.load_data()
+
+x_train = tf.keras.utils.normalize(x_train, axis=1)
+x_test = tf.keras.utils.normalize(x_test, axis=1)
+
+
+def trainingAI(number_epochs):
+    model = tf.keras.models.Sequential()
+    model.add(tf.keras.layers.Flatten(input_shape=(28, 28)))
+    model.add(tf.keras.layers.Dense(128, activation='relu'))
+    model.add(tf.keras.layers.Dense(128, activation='relu'))
+    model.add(tf.keras.layers.Dense(10, activation='softmax'))
+
+    model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
+
+    model.fit(x_train, y_train, epochs=number_epochs)
+
+    model.save("handwritten.model")
+
+
+def loadmodel():
+    model = tf.keras.models.load_model("handwritten.model")
+    img = cv2.imread(f"digits/digits.png")[:,:,0]
+    img = np.invert(np.array([img]))
+    prediction = model.predict(img)
+    print(f"To jest liczba{ np.argmax(prediction)}")
+    plt.imshow(img[0], cmap=plt.cm.binary)
+    plt.show()
+
+
+if __name__ == "__main__":
+    trainingAI(5)
+    loadmodel()
